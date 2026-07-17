1ef4e5591e26c2f25b1f16c04cf1bfe52d011296
colincummins <all.our.august.sundays@gmail.com>
2026-07-17 13:06:26 -0700

    PdfSplitter framework

---
+++ app/src/main/java/com/reedelectric/apmailer/PdfSplitter.java
@@ -0,0 +1,11 @@
+package com.reedelectric.apmailer;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class PdfSplitter {
+    void split(Path inputPath, Path outputPath) throws IOException {
+
+    }
+
+}
