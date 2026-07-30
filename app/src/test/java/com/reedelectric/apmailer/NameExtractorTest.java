38cb67e84d464513ffa0558f04bf5986d2ff1336
colincummins <all.our.august.sundays@gmail.com>
2026-07-30 14:12:07 -0700

    WIP - Now able to extract text from PDF

--- app/src/test/java/com/reedelectric/apmailer/NameExtractorTest.java
+++ app/src/test/java/com/reedelectric/apmailer/NameExtractorTest.java
@@ -1,10 +1,22 @@
 package com.reedelectric.apmailer;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.jupiter.api.Test;
 
 public class NameExtractorTest {
     @Test
-    void displayContents() {
+    void displayContents() throws IOException {
+        File pdfFile = new File("src/test/resources/single_page_statement.pdf");
+        try (PDDocument target = Loader.loadPDF(pdfFile)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            System.out.println(stripper.getText(target));
+
+        }
 
     }
 
