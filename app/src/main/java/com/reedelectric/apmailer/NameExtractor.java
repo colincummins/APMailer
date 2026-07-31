406c467587a54df5f8b9e45a21db38191ee4ccef
colincummins <all.our.august.sundays@gmail.com>
2026-07-31 14:04:28 -0700

    NameExtractor extracts name from pdf, or null if no name available. Testing for same.

--- app/src/main/java/com/reedelectric/apmailer/NameExtractor.java
+++ app/src/main/java/com/reedelectric/apmailer/NameExtractor.java
@@ -1,5 +1,29 @@
 package com.reedelectric.apmailer;
 
-public class NameExtractor {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class NameExtractor {
+    private static final Pattern NAME_PATTERN = Pattern.compile("Customer ID:\\R(.+)\\R");
+
+    public String extract(Path pdfPath) throws IOException {
+        try (PDDocument target = Loader.loadPDF(pdfPath.toFile())) {
+            return extract(target);
+        }
+    }
+
+    public String extract(PDDocument document) throws IOException {
+        PDFTextStripper stripper = new PDFTextStripper();
+        Matcher m = NAME_PATTERN.matcher(stripper.getText(document));
+        if (m.find()) {
+            return m.group(1).trim();
+        }
+        return null;
+    }
 }
--- app/src/test/java/com/reedelectric/apmailer/NameExtractorTest.java
+++ app/src/test/java/com/reedelectric/apmailer/NameExtractorTest.java
@@ -3,22 +3,25 @@ package com.reedelectric.apmailer;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 public class NameExtractorTest {
     @Test
-    void displayContents() throws IOException {
+    void extractsName() throws IOException {
         File pdfFile = new File("src/test/resources/single_page_statement.pdf");
-        try (PDDocument target = Loader.loadPDF(pdfFile)) {
-            PDFTextStripper stripper = new PDFTextStripper();
-            System.out.println(stripper.getText(target));
-
-        }
-
+        NameExtractor extractor = new NameExtractor();
+        String name = extractor.extract(pdfFile.toPath());
+        assertEquals("KINDER MORGAN (4) BULK TERM INC", name, "Wrong/no name extracted" );
     }
 
+    @Test
+    void extractsNull() throws IOException {
+        File pdfFile = new File("src/test/resources/blank.pdf");
+        NameExtractor extractor = new NameExtractor();
+        String name = extractor.extract(pdfFile.toPath());
+        assertNull(name, "Extracting customer name from PDF with no name should return null");
+    }
 
 }
